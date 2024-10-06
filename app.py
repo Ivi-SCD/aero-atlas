9c1ccc25589e79b90e68af16b70ceb1dc448e2df
Ivisson Pereira <ivipnascimento@hotmail.com>
2024-10-06 14:59:12 -0300

    Flask port binding

--- app.py
+++ app.py
@@ -99,3 +99,7 @@ def register():
 @app.route('/analysis/')
 def analysis():
     return render_template('analysis.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
